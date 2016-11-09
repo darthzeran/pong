973d358a5c76373e2861fcf264f6367777bf5d5f
darthzeran <ryan.mort@studentbody.byu.edu>
2016-11-09 14:19:45 -0700

    Add files via upload

---
+++ public/javascripts/comments.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	
+	$.getJSON('comment', function(data) {
+       // console.log(data);
+        var everything = "<ul>";
+        for(var comment in data) {
+          com = data[comment];
+          everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+        }
+        everything += "</ul>";
+        $("#comments").html(everything);
+      })
+	
+   $("#serialize").click(function(){
+   var myobj = {Name:$("#Name").val(),Comment:$("#Comment").val()};
+    jobj = JSON.stringify(myobj);
+   // $("#json").text(jobj);	
+	var url = "comment";
+$.ajax({
+  url:url,
+  type: "POST",
+  data: jobj,
+  contentType: "application/json; charset=utf-8",
+  success: function(data,textStatus) {
+     // $("#done").html(textStatus);
+  }
+})
+
+ $.getJSON('comment', function(data) {
+       // console.log(data);
+        var everything = "<ul>";
+        for(var comment in data) {
+          com = data[comment];
+          everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+        }
+        everything += "</ul>";
+        $("#comments").html(everything);
+      })
+    })
+});
+/*
+
+$scope.clear = function() {
+           $http({
+  method: 'GET',
+  url: '/delete'
+}).then(function successCallback(response) {
+
+  }, function errorCallback(response) {
+  });
+        console.log('deleting all');
+          $scope.getAll();
+    };
+
+
+*/
+	
+
+
